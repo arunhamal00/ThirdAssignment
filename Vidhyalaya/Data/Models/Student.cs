5d830fda711a91eec2760ab663d0f25d4b07fe6d
Arun Hamal <thakuriarun565@gmail.com>
2024-05-21 21:38:20 +0545

    files for vidhyalaya project

--- Vidhyalaya/Data/Models/Student.cs
+++ Vidhyalaya/Data/Models/Student.cs
@@ -1,8 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 public class Student
 {
     public int Id { get; set; }
     public String Name { get; set; }
     public string Address { get; set; }
+    [Display(Name = "Gender")]
     public Gender Sex { get; set; }
     public DateTime Dob { get; set; }
     public byte[] Photo { get; set; }
@@ -12,8 +15,12 @@ public class Student
 
 public enum Gender
 {
-    Male,
-    Female,
-    Others
+    [Display(Name = "Male")]
+    Male = 1,
 
+    [Display(Name = "Female")]
+    Female,
+
+    [Display(Name = "Others")]
+    Others
 }
